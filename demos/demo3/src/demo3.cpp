3383cb32b58c4694ebe2cb25ebd731c1cf2a07a6
karlvonbahnhof <karlvonbahnhof@e5b3dcb2-1d4a-0410-9aef-e370f01ccb3f>
2008-10-13 09:17:45 +0000

    makefile pro demo3
    git-svn-id: svn://svn.code.sf.net/p/bombic2/code@60 e5b3dcb2-1d4a-0410-9aef-e370f01ccb3f

---
+++ demos/demo3/src/demo3.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
+
+using namespace std;
+
+#include "SDL_lib.h"
+#include "game_intro.h"
+
+Fonts g_font("fonts/verdana.ttf");
+GameIntro g_gameIntro;
+SDL_Surface *g_window=0;
+
+int main(int argc, char ** argv){
+	srand(time(0));
+
+	int return_val=0;
+	try {
+		window_init(&g_window, 800, 600, "Bombic2 - Demo 2");
+
+		g_gameIntro.new_game(1, 1);
+		g_gameIntro.show_screen();
+
+	}
+	catch(int val){
+		return_val = val;
+	}
+	if(g_window) SDL_FreeSurface(g_window);
+	return return_val;
+}
+
