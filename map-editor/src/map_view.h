11022fb5c8f5c365c0bea46ec0f70b1735f2e9f5
karlvonbahnhof <karlvonbahnhof@e5b3dcb2-1d4a-0410-9aef-e370f01ccb3f>
2010-03-27 21:03:07 +0000

    Okomentovan header MapView.
    git-svn-id: svn://svn.code.sf.net/p/bombic2/code@192 e5b3dcb2-1d4a-0410-9aef-e370f01ccb3f

--- map-editor/src/map_view.h
+++ map-editor/src/map_view.h
@@ -1,4 +1,6 @@
 /** @file map_view.h
+ * Pohled na mapu MapView je widget,
+ * ktery zastresuje praci s mapou.
  */
 
 #ifndef MAP_VIEW_H_GUARD_
@@ -8,6 +10,7 @@
 #include <QPixmap>
 #include <singleton.h>
 
+/// Zkratka pro ziskani instance singletonu MapView.
 #define MAP_VIEW SINGLETON_SHORTCUT(MapView)
 
 class QGraphicsView;
@@ -20,6 +23,12 @@ class MapScene;
 class BombicMapBackground;
 class BombicMapObject;
 
+/** Pohled na mapu a prilehle widgety.
+ * Centralnim prvkem je QGraphicsView, ktere zobrazuje mapu.
+ * Dale ZoomWidget, a QLabel zobrazujici pracovni objekt.
+ * V ramci MapView je podporovan drag and drop na objekty mapy.
+ * TODO
+ */
 class MapView: public QWidget {
 
 	Q_OBJECT
@@ -28,33 +37,46 @@ class MapView: public QWidget {
 
 
 	public:
+		/// Zkonstruuje pohled.
 		explicit MapView(QWidget * parent = 0);
-
+		/// Destruuje pohled.
 		~MapView();
-
+		/// Layout pohledu.
 		QGridLayout * gridLayout();
 
+		/// Zobrazit obrazek pracovniho objektu.
 		void showWorkingObjectLabel(const QPixmap & objectPixmap);
+		/// Schovat obrazek pracovniho objektu.
 		void hideWorkingObjectLabel();
 
+		/// Vytvorit mime data, ktera prenasi objekt.
 		static QMimeData * createMimeData(BombicMapObject * object);
+		/// Ziskat objekt z mime dat, ktera ho prenasi.
 		static BombicMapObject * getMapObject(
 				const QMimeData * mimeData);
 
 	public slots:
+		/// Nastavi zoom mapy.
 		void setZoom(qreal zoomQuotient);
 
 	signals:
+		/// Pohled opusten.
 		void leaved();
 
 	protected:
+		/// Handler oppusteni pohledu.
 		virtual void leaveEvent(QEvent * event);
 
 	private:
+		/// Pohled na scenu mapy.
 		QGraphicsView * viewport_;
+		/// Scena mapy.
 		MapScene * scene_;
+		/// Zoomovaci widget.
 		ZoomWidget * zoomWidget_;
+		/// Aktualni (posledni nastaveny) zoom.
 		qreal lastZoomQuotient_;
+		/// Obrazek pracovniho objektu.
 		QLabel * workingObjectLabel_;
 
 		// dalsi prvky jako fieldview, viewtoolbar
